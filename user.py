b340d5af8d4f623beec172c91bb5b6c8331f4186
eshitasp20 <eshitasp20@gmail.com>
2026-09-04 10:19:18 +0530

    saving the file to third branch

---
+++ user.py
@@ -0,0 +1,8 @@
+id = input("Enter employeeID")
+name = input("Enter your name")
+Lname = input("Enter your last name")
+contact = input("Enter your contact")
+email = input("Enter the email")
+city = input("Enter the city")
+
+print("Your detail",name,Lname)
